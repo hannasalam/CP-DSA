8aef6cf5175ed09c8023bbefc51bd1392f771f83
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2021-09-06 06:11:39 +0530

    Leetcode - DP

---
+++ DSA/maximum subarray.cpp
@@ -0,0 +1,14 @@
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, sax = INT_MIN;
+        for(int num:nums)
+        {
+            sum+=num;
+            sax = max(sum,sax);
+            if(sum<0)
+            {
+                sum = 0;
+            }
+        }
+        return sax;
+        
+    }
