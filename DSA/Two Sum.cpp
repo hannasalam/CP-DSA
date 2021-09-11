e74ffd0481ba6cd573c4e8c7f900d65600d4c403
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2021-09-11 08:34:44 +0530

    Leetcode - Hashmaps

---
+++ DSA/Two Sum.cpp
@@ -0,0 +1,10 @@
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(nums[i])!=mp.end()) 
+                return {i,mp[nums[i]]};
+            mp[target-nums[i]] = i;
+        }
+        return {};
+    }
