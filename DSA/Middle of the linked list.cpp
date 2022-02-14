3bae9d66f85dfd9d20dc914f08eb96d8f0dff314
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2022-02-14 07:41:40 +0530

    Leetcode - Linked list

---
+++ DSA/Middle of the linked list.cpp
@@ -0,0 +1,28 @@
+Not so optmized:
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int len =0;
+        ListNode *copy = head;
+        while(copy!=nullptr)
+        {
+            len++;
+            copy = copy->next;
+        }
+        int mid;
+        if(len&1)
+            mid = ceil(len/2.0);
+        else
+            mid = len/2+1;
+        cout<<mid;
+        for(int i=1;i<mid;i++)
+        {
+            head = head->next;
+        }
+        return head;
+        
+    }
+};
+
+Better one:
