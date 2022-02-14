031a457bc0ba138cdba426c8c34e9a4a04be67dc
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2022-02-14 07:43:31 +0530

    Leetcode- LInked list | Tortoise and Hare Algo

--- DSA/Middle of the linked list.cpp
+++ DSA/Middle of the linked list.cpp
@@ -26,3 +26,18 @@ public:
 };
 
 Better one:
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow= head;
+        ListNode *fast = head;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+        
+    }
+};
