06b660f29dd29a462460ff546cfc8c8f505263d9
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2022-07-31 11:04:35 +0530

    optimized linked list

--- DSA/Intersection of 2 linked lists.cpp
+++ DSA/Intersection of 2 linked lists.cpp
@@ -33,3 +33,15 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         return NULL;
         
     }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA;
+        ListNode *b = headB;
+        while(a!=b)
+        {
+            a = a==NULL?headB:a->next;
+            b = b==NULL?headA:b->next;
+        }
+        return a;
+        
+    }
