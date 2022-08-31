74209f1faeda05e820f46a97a5744c83d4d4078d
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2022-08-31 20:08:38 +0530

    acyclic directed graph

---
+++ DSA/Graph/TopologicalSort.cpp
@@ -0,0 +1,31 @@
+vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int> topo;
+	    int indegree[V] = {0};
+	    for(int i=0;i<V;i++)
+	    {
+	        for(auto j:adj[i])
+	        {
+	            indegree[j]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i=0;i<V;i++)
+	    {
+	       if(indegree[i]==0)
+	                q.push(i);
+	    }
+	    while(!q.empty())
+	    {
+	        int x = q.front();
+	        q.pop();
+	        topo.push_back(x);
+	        for(int i:adj[x])
+	        {
+	            indegree[i]--;
+	            if(indegree[i]==0)
+	                q.push(i);
+	        }
+	    }
+	    return topo;
+	}
