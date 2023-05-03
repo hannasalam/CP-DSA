e22f09386b291b21dcf9850f3fca4b4cfd6d64dc
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2023-05-03 08:01:33 +0530

    better qn leetcode

--- DSA/Heap/kth largest element.cpp
+++ DSA/Heap/kth largest element.cpp
@@ -1,17 +1,8 @@
-class KthLargest {
-public:
-    int kk;
-    priority_queue<int, vector<int>, greater<int>> heap;
-    KthLargest(int k, vector<int>& nums) {
-        kk = k;
+int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int> , greater<int>> hp;
         for(int i=0;i<nums.size();i++){
-            heap.push(nums[i]);
-            if(heap.size()>k) heap.pop();
+            hp.push(nums[i]);
+            if(hp.size()>k) hp.pop();
         }
-    }
-    int add(int val) {
-        heap.push(val);
-        if(heap.size()>kk) heap.pop();
-        return heap.top();
-    }
-};
+        return hp.top();
+}
