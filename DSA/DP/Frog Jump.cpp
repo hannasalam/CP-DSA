71ef4504dce64b89ef39b93a23f19993f8319e16
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2023-06-08 07:34:42 +0530

    GFG  better approaches

--- DSA/DP/Frog Jump.cpp
+++ DSA/DP/Frog Jump.cpp
@@ -16,3 +16,32 @@ int frogJump(int n, vector<int> &heights)
     vector<int> dp(n,-1);
     return findEnergyLost(dp, n-1, heights);
 }
+
+//tabulization
+int minimumEnergy(vector<int>& height, int n) {
+        vector<int> dp(n,-1);
+        // return solve(height,n-1,dp);
+        dp[0] = 0;
+        dp[1] = abs(height[1]-height[0]);
+        for(int i=2;i<n;i++){
+            dp[i] = min(dp[i-1]+abs(height[i] - height[i-1]), dp[i-2]+abs(height[i] - height[i-2]));
+        }
+       return dp[n-1];
+}
+
+//tabulization constant space
+
+    int minimumEnergy(vector<int>& height, int n) {
+        vector<int> dp(n,-1);
+        // return solve(height,n-1,dp);
+        int f = 0;
+        if(n==1) return 0;
+        int s = abs(height[1]-height[0]);
+        for(int i=2;i<n;i++){
+            int t = min(s+abs(height[i] - height[i-1]), f+abs(height[i] - height[i-2]));
+            f = s;
+            s = t;
+            
+        }
+       return s;
+    }
