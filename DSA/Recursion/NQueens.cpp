56d0eafbd1dd7bb1700abc078266b882049f4cf8
Hanna Salam <74866650+hannasalam@users.noreply.github.com>
2023-08-03 06:28:44 +0530

    better code for nqueens

--- DSA/Recursion/NQueens.cpp
+++ DSA/Recursion/NQueens.cpp
@@ -35,3 +35,37 @@ public:
         return true;
     }
 };
+
+//elegant code
+
+class Solution {
+public:
+    bool placable(int row,int col,vector<vector<int>> &matrix){
+        for(int r=0;r<row;r++){
+            if(matrix[r][col]==1) return false;
+            int rowDiff = row-r;
+            if(col-rowDiff>=0 and matrix[r][col-rowDiff]==1) return false;
+            if(col+rowDiff<matrix.size() and matrix[r][col+rowDiff]==1) return false;
+        }
+        return true;
+    }
+    void ans(int n,int row,vector<vector<int>> &matrix,int& res){
+        if(row==n){
+            res++;
+            return;
+        }
+        for(int col = 0;col<n;col++){
+            if(placable(row,col,matrix)){
+                matrix[row][col] = 1;
+                ans(n,row+1,matrix,res);
+                matrix[row][col] = 0;
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        int res = 0;
+        vector<vector<int>> matrix(n,vector<int>(n,0));
+        ans(n,0,matrix,res);
+        return res;
+    }
+};
